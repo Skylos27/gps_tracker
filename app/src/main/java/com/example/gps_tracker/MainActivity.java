a3284ed1fa045be1951ed9bd9facec4ad548c569
Skylos27 <antoine.auger@epita.fr>
2021-05-10 17:59:57 +0100

    Add Pos listener

--- app/src/main/java/com/example/gps_tracker/MainActivity.java
+++ app/src/main/java/com/example/gps_tracker/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.gps_tracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+
 
 public class MainActivity extends AppCompatActivity {
+    private LocationManager locationManager;
+    private LocationListener listener;
+    private double xcoord ;
+    private double ycoord;
+    private double altitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    private void getLocation(){
+        Context context = getApplicationContext();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                xcoord = location.getLatitude();
+                ycoord = location.getLongitude();
+                altitude = location.getAltitude();
+            }
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
     }
 }
---
+++ app/src/main/java/com/example/gps_tracker/ReportActivity.java
@@ -0,0 +1,6 @@
+package com.example.gps_tracker;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class ReportActivity extends FragmentActivity {
+}
