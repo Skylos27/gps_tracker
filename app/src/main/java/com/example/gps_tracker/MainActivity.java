81a859d3939724f2fb4fca38bf14c32c9bcde009
Antoine Auger <antoine.auger@epita.fr>
2021-05-18 05:08:26 +0100

    Creation of the GPX files implemented

--- app/src/main/java/com/example/gps_tracker/MainActivity.java
+++ app/src/main/java/com/example/gps_tracker/MainActivity.java
@@ -18,6 +18,7 @@ import android.provider.DocumentsContract;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,9 +26,11 @@ import android.widget.Toast;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
@@ -55,22 +58,21 @@ public class MainActivity extends AppCompatActivity {
     String totaltime;
     String filename;
     String path;
-    File myFile;
+    public File myFile;
     String formattedDate;
+    Double[] distCalculation;
+    Button onOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat df = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss");
-        formattedDate = df.format(c.getTime());
-        filename = formattedDate;
-        path = Environment.getExternalStorageDirectory().toString() + "/GPStracks/"+filename+".gpx";
+
         locMan = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
         chrono = findViewById(R.id.chronoMeter);
         lat = findViewById(R.id.latitudeText);
         lon = findViewById(R.id.longitudeText);
+        onOff = findViewById(R.id.startButton);
     }
 
     private double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
@@ -84,13 +86,15 @@ public class MainActivity extends AppCompatActivity {
         return (dist);
     }
 
+    private void totalDistance(){
 
+    }
     //  This function converts decimal degrees to radians
     private double deg2rad(double deg) {
         return (deg * Math.PI / 180.0);
     }
     public void checkMyPermissions(View view){
-        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)== PackageManager.PERMISSION_DENIED){
+
             if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION))
                 requestPermissions(new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, PackageManager.PERMISSION_GRANTED);
 
@@ -107,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
 
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
-        }
+
 
     }
 
@@ -117,109 +121,122 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void recording(View view) {
-        isOn = true;
-        createGPX();
-        timer();
-        checkMyPermissions(view);
-        createLocationListener();
-    }
+
+        if(!isOn){
+            createGPX();
+            timer();
+            checkMyPermissions(view);
+            isOn = true;
+            createLocationListener();
+            onOff.setText("Stop");
 
 
-    @Override
-    protected void onStop(){
-        super.onStop();
-        try {
-            FileOutputStream f = new FileOutputStream(myFile);
-            PrintWriter pw = new PrintWriter(f);
-            pw.println("</gpx>");
-            pw.flush();
-            pw.close();
-            f.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            Log.i(TAG, "******* File not found. Did you" +
-                    " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        else{
+            isOn = false;
+            chrono.stop();
+            try {
+                FileWriter fw = new FileWriter(myFile,true);
+                BufferedWriter bw = new BufferedWriter(fw );
+                PrintWriter pw = new PrintWriter(bw);
+                pw.println("</gpx>");
+                pw.flush();
+                pw.close();
+                bw.close();
+                fw.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Log.i(TAG, "Impossible to put the end");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        locMan = null;
+            locMan = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        }
     }
 
     private void createLocationListener(){
-        try{
-            locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
 
-                    latitude = location.getLatitude();
-                    longitude = location.getLongitude();
-                    double alti = location.getAltitude();
-                    lon.setText("Current longitude: "+longitude);
-                    lat.setText("Current latitude: "+latitude);
-                    try {
-                        FileOutputStream f = new FileOutputStream(myFile);
-                        PrintWriter pw = new PrintWriter(f);
-                        pw.println("<wpt lat=\""+latitude+"\" lon=\""+longitude+"\">\n" +
-                                "    <ele>"+alti+"</ele>\n" +
-                                "    <time>"+formattedDate+"</time>\n"+
-                                "  </wpt>");
-                        pw.flush();
-                        pw.close();
-                        f.close();
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                        Log.i(TAG, "******* File not found. Did you" +
-                                " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
-                    } catch (IOException e) {
-                        e.printStackTrace();
+            try {
+                locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        double alti = location.getAltitude();
+                        if(isOn) {
+                            lon.setText("Current longitude: " + longitude);
+                            lat.setText("Current latitude: " + latitude);
+                        }
+
+                        try {
+                            if (isOn) {
+                                FileWriter fw = new FileWriter(myFile, true);
+                                BufferedWriter bw = new BufferedWriter(fw);
+                                PrintWriter pw = new PrintWriter(bw);
+                                pw.println("<wpt lat=\"" + latitude + "\" lon=\"" + longitude + "\">\n" +
+                                        "    <ele>" + alti + "</ele>\n" +
+                                        "    <time>" + formattedDate + "</time>\n" +
+                                        "  </wpt>");
+                                pw.flush();
+                                pw.close();
+                                bw.close();
+                                fw.close();
+                            }
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                            Log.i(TAG, "Impossible to add points");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
                     }
 
-                }
+                    @Override
+                    public void onProviderEnabled(String provider) {
 
-                @Override
-                public void onProviderEnabled(String provider){
-
-                    if(provider == LocationManager.GPS_PROVIDER){
-                        //Show last known.
-                        Location loc = locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (provider == LocationManager.GPS_PROVIDER) {
+                            //Show last known.
+                            Location loc = locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
 
-                        if(loc != null) {
-                            //Now if the location is not null just show it.
-                            lat.setText("" + loc.getLatitude());
-                            lon.setText("" + loc.getLongitude());
-                        }//inner if
+                            if (loc != null) {
+                                //Now if the location is not null just show it.
+                                lat.setText("" + loc.getLatitude());
+                                lon.setText("" + loc.getLongitude());
+                            }//inner if
 
-                    }//outer if
+                        }//outer if
 
-                }//onProviderEnabled()
+                    }//onProviderEnabled()
 
-                @Override
-                public void onProviderDisabled(String provider){
+                    @Override
+                    public void onProviderDisabled(String provider) {
 
-                    if(provider != LocationManager.GPS_PROVIDER){
-                        //Just show default string.
-                        lat.setText("Currently unavailable");
-                        lon.setText("Currently unavailable");
+                        if (provider != LocationManager.GPS_PROVIDER) {
+                            //Just show default string.
+                            lat.setText("Currently unavailable");
+                            lon.setText("Currently unavailable");
 
-                    }//if
+                        }//if
 
-                }//onProviderDisabled()
+                    }//onProviderDisabled()
 
 
-                public void onStatusChanged(String provider, int status, Bundle extras){
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-                }//onStatusChanged()
+                    }//onStatusChanged()
 
-            });
+                });
 
-        }// try
-        catch(SecurityException se){
-            se.printStackTrace();
-        }//catch
+            }// try
+            catch (SecurityException se) {
+                se.printStackTrace();
+            }//catch
+
     }
-    // Timer for the UI
+     //Timer for the UI
     private void timer(){
         chrono.setBase(SystemClock.elapsedRealtime());
         chrono.start();
@@ -227,21 +244,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createGPX(){
-        myFile = new File(path);
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss");
+        formattedDate = df.format(c.getTime());
+        filename = formattedDate;
+        File folder = new File(Environment.getExternalStorageDirectory() +
+                File.separator +  "GPXtracks/");
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        myFile = new File(Environment.getExternalStorageDirectory()+File.separator +  "GPXtracks/", filename+".gpx");
         try {
-            FileOutputStream f = new FileOutputStream(myFile);
-            PrintWriter pw = new PrintWriter(f);
+            FileWriter fw = new FileWriter(myFile,true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
             pw.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\n" +
                     "<gpx xmlns=\"http://www.topografix.com/GPX/1/1\" creator=\"byHand\" version=\"1.1\" \n" +
                     "    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \n" +
                     "    xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\">");
             pw.flush();
             pw.close();
-            f.close();
+            bw.close();
+            fw.close();
+            Log.i(TAG,myFile.getAbsolutePath());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            Log.i(TAG, "******* File not found. Did you" +
-                    " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
+            Log.i(TAG, "Impossible to create the file");
         } catch (IOException e) {
             e.printStackTrace();
         }
